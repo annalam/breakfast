2966597277106a20df60fe64a1c0b877d6bb4e50
Gnanavel Mutharasu <gnanavel.mutharasu@gmail.com>
2017-06-05 17:37:26 +0300

    lib.rs for extra file and path handling features

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use std::path::Path;
+use std::process::Command;
+
+pub fn mkdir(path: &String) {
+  if Path::new(path).exists(){
+     println!("{:?} already exists", path);
+  } else {
+    Command::new("mkdir")
+            .arg(path)
+            .spawn()
+            .expect("failed to mkdir");
+  }
+}
+
+pub fn zopen(path: &String, mode: String) {
+      
+}
